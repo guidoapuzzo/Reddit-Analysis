3c4c595ad85acf4a8e353bd7d17434eb8f4394cc
Guido Apuzzo <98398020+guidoapuzzo@users.noreply.github.com>
2024-07-11 17:29:04 +0200

    Update reddit_credentials.py

--- reddit_credentials.py
+++ reddit_credentials.py
@@ -2,7 +2,7 @@
 Defines the credentials to authenticate and authorise the client to make requests to the Reddit API.
 '''
 
-CLIENT_ID = "jHDOalUVXYN_1xcWod4gJQ"
-SECRET_TOKEN = "oV0dOIWzlBaf69kppb7vWLSqUXg-cg"
-USERNAME = "willbeok-1507"
-PASSWORD = "^6uw1uU9kAubYxrtm2Lk"
+CLIENT_ID = " "
+SECRET_TOKEN = " "
+USERNAME = " "
+PASSWORD = " "
